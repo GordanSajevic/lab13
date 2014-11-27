b8e3a9972833da9c7f7290d04fcb24921b4341c2
Gordan Sajević <gsajevic@hotmail.com>
2014-11-27 11:34:13 +0100

    Update Koordinate.java

--- Koordinate.java
+++ Koordinate.java
@@ -3,12 +3,19 @@ package lab13;
 public class Koordinate {
 
 	public static void main(String[] args) {
-		for (int i=0; i<3; i++)
+		int[] niz = new int[5];
+		int visina = 5, duzina = 3;
+		int[][] matrica = new int[visina][];
+		
+		for (int i=0; i<matrica.length; i++)
 		{
-			
-			for (int j=0; j<3; j++)
+			matrica[i] = new int [visina - i];
+		}
+		for (int i=0; i<matrica.length; i++)
+		{
+			for (int j=0; j < matrica[i].length; j++)
 			{
-				System.out.printf("%d,%d  ", i, j);
+				System.out.print(matrica[i][j] + " ");
 			}
 			System.out.println();
 		}
@@ -16,3 +23,4 @@ public class Koordinate {
 	}
 
 }
+
