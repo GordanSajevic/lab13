90e1151ea695be667947d55b16f49810efbf674a
Gordan Sajević <gsajevic@hotmail.com>
2014-11-27 10:55:28 +0100

    Create Koordinate.java

---
+++ Koordinate.java
@@ -0,0 +1,18 @@
+package lab13;
+
+public class Koordinate {
+
+	public static void main(String[] args) {
+		for (int i=0; i<3; i++)
+		{
+			
+			for (int j=0; j<3; j++)
+			{
+				System.out.printf("%d,%d  ", i, j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
